00f2dce3a56b5cc484b657f1ca19e967dedf7e06
Kevin Ludwig <ludwig_kevin@outlook.com>
2020-08-01 09:18:52 +0200

    extending port reader with timestamps and keyhandling

--- DataReader.py
+++ DataReader.py
@@ -1,24 +1,38 @@
 import serial
-import time
 import datetime
+import sys
+import keyboard
 
-ser = serial.Serial(
+try:
+    ser = serial.Serial(
     port='COM3',\
     baudrate=9600,\
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
     bytesize=serial.EIGHTBITS,\
-        timeout=1)
+    timeout=1)
+    print("Connected to COM 3")
+except serial.SerialException:
+    print("Error while connecting to port COM 3")
+    sys.exit(0)
+
+# current time for calculating the time values for incoming data
+start_time = datetime.datetime.now()
 
 while True:
-    # reading is a string
     data = ser.readline()
     data_encode = data.decode('ascii').strip()
-    print(data_encode)
 
+    # Timestamp in seconds for every single data value
+    time_diff = (datetime.datetime.now() - start_time)
+    execution_time = str(time_diff.total_seconds())
+
+    # Write data into textfile
     with open('output.txt', 'a') as text_file:
-        text_file.write(str(data_encode) + "\n")
+        text_file.write(execution_time + " " + str(data_encode) + "\n")
 
-
+    # Break the loop for exiting the program via keyhandling
+    if keyboard.is_pressed('s'):
+        break
 
 
